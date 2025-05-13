8a5ea397320ecf47eaae5df45637f89c25f1741b
unknown <dridiomar615@gmail.com>
2025-05-12 17:28:25 -0700

    test7 update4

---
+++ .next/static/5SYHZCoc7tBenXX9wMX-O/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fabout","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fcart","\u002F[locale]\u002Fcontact","\u002F[locale]\u002Forders","\u002F[locale]\u002Fplans","\u002F[locale]\u002Fpricing"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
